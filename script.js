9ba47bd36687fdd6665c6924c3ce256abf492800
Kiranpreet Kaur <kiranpreetkaursandhu244@gmail.com>
2025-11-23 22:41:19 +0530

    Publication images

--- script.js
+++ script.js
@@ -20,3 +20,12 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
 
     this.reset();
 });
+
+    window.addEventListener("scroll", function () {
+        const nav = document.querySelector(".navbar");
+        if (window.scrollY > 10) {
+            nav.classList.add("scrolled");
+        } else {
+            nav.classList.remove("scrolled");
+        }
+    });
